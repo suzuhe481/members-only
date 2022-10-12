5f3becd5c3bac7566eedca89914c28b59b5b9d06
Hector <baseballhector@gmail.com>
2022-10-12 14:49:26 -0400

    Updated with Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_001513) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_184500) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
