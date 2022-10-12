0da0fadac0648e1ae6b359ae7554b8856a8b104b
Hector <baseballhector@gmail.com>
2022-10-12 16:27:12 -0400

    #new, #create, #index, #post_params methods added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
   before_action :new
   before_action :create
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    # @post.user = current_user
+
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.permit(:body)
+  end
 end
